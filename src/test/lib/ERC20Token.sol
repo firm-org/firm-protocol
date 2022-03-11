73b5402a903e77918f0a10039395feed4576b614
Jorge Izquierdo <j@firm.org>
2022-03-11 13:31:06 +0100

    test: integration

---
+++ src/test/lib/ERC20Token.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import "solmate/tokens/ERC20.sol";
+
+contract ERC20Token is ERC20 {
+    constructor() ERC20("", "", 18) {}
+    
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
