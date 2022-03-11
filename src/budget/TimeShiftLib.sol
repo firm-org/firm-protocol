9c156f5c500d4329aea6cc27cedd551ddb74e583
Jorge Izquierdo <j@firm.org>
2022-03-11 10:42:31 +0100

    refactor: shifts

--- src/budget/TimeShiftLib.sol
+++ src/budget/TimeShiftLib.sol
@@ -33,65 +33,38 @@ library TimeShiftLib {
         (uint256 y, uint256 m, uint256 d) = realTime.toDate();
         TimeUnit unit = shift.unit;
 
-        uint256 daysToAdd = 0;
         if (unit == TimeUnit.Daily) {
-            daysToAdd = 1;
+            (y, m, d) = addDays(y, m, d, 1);
         } else if (unit == TimeUnit.Weekly) {
-            daysToAdd = 8 - DateTimeLib.getDayOfWeek(realTime);
+            (y, m, d) = addDays(y, m, d, 8 - DateTimeLib.getDayOfWeek(realTime));
         } else if (unit == TimeUnit.Monthly) {
-            d = 1;
-            if (m < 12) {
-                m += 1;
-            } else {
-                m = 1;
-                y += 1;
-            }
+            (y, m, d) = m < 12 ? (y, m + 1, 1) : (y + 1, 1, 1);
         } else if (unit == TimeUnit.Quarterly) {
-            d = 1;
-            if (m < 10) {
-                m = (1 + (m - 1) / 3) * 3 + 1;
-            } else {
-                m = 1;
-                y += 1;
-            }
+            (y, m, d) = m < 10 ? (y, (1 + (m - 1) / 3) * 3 + 1, 1) : (y + 1, 1, 1);
         } else if (unit == TimeUnit.Semiyearly) {
-            d = 1;
-            if (m < 7) {
-                m = 7;
-            } else {
-                m = 1;
-                y += 1;
-            }
+            (y, m, d) = m < 7 ? (y, 7, 1) : (y + 1, 1, 1);
         } else if (unit == TimeUnit.Yearly) {
-            d = 1;
-            m = 1;
-            y += 1;
+            (y, m, d) = (y + 1, 1, 1);
         } else {
             revert UnknownTimeShift();
         }
 
-        if (daysToAdd > 0) {
-            uint256 daysInMonth = DateTimeLib._getDaysInMonth(y, m);
-            uint256 potentialDay = d + daysToAdd;
-
-            if (potentialDay <= daysInMonth) {
-                d = potentialDay;
-            } else {
-                d = potentialDay - daysInMonth;
-                if (m < 12) {
-                    m += 1;
-                } else {
-                    m = 1;
-                    y += 1;
-                }
-            }
-        }
-
-        // if (initialTime >= shiftedTimestamp) revert BadShift(); SHOULD NEVER BE THE CASE
-
         uint256 shiftedTs = DateTimeLib.timestampFromDateTime(y, m, d, 0, 0, 0);
-
         return uint64(int64(uint64(shiftedTs)) - shift.offset);
+    }
+
+    /**
+     * @dev IT WILL ONLY TRANSITION ONE MONTH IF NECESSARY
+     */
+    function addDays(uint256 y, uint256 m, uint256 d, uint256 daysToAdd) private pure returns (uint256, uint256, uint256) {
+        uint256 daysInMonth = DateTimeLib._getDaysInMonth(y, m);
+        uint256 d2 = d + daysToAdd;
+
+        return d2 <= daysInMonth
+            ? (y, m, d2)
+            : m < 12
+                ? (y, m + 1, d2 - daysInMonth)
+                : (y + 1, 1, d2 - daysInMonth);
     }
 
     function toDate(uint64 timestamp)
