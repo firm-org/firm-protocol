a794a707a43c1036262932cc8fe8d98ade2a7c86
Jorge Izquierdo <j@firm.org>
2022-03-17 10:34:37 +0100

    Factory (#2)
    * factory: initial implementation
    
    * factory: delete duped file

---
+++ src/factory/test/FirmFactoryIntegrationTest.t.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import "solmate/test/utils/DSTestPlus.sol";
+
+import "gnosis-safe/GnosisSafe.sol";
+import "gnosis-safe/proxies/GnosisSafeProxyFactory.sol";
+import "gnosis-safe/common/Enum.sol";
+import "zodiac/factory/ModuleProxyFactory.sol";
+
+import "./lib/ERC20Token.sol";
+
+import {FirmFactory} from "../FirmFactory.sol";
+import {Budget, TimeShiftLib} from "../../budget/Budget.sol";
+import {Roles} from "../../roles/Roles.sol";
+
+contract FirmFactoryIntegrationTest is DSTestPlus {
+    using TimeShiftLib for *;
+
+    FirmFactory factory;
+    ERC20Token token;
+
+    function setUp() public {
+        token = new ERC20Token();
+        factory = new FirmFactory(
+            new GnosisSafeProxyFactory(),
+            new ModuleProxyFactory(),
+            address(new GnosisSafe()),
+            address(new Roles(address(10))),
+            address(new Budget(Budget.InitParams(address(10), address(10), address(10), address(10))))
+        );
+    }
+
+    function testFactoryGas() public {
+        factory.createFirm(address(this));
+    }
+
+    function testInitialState() public {
+        (GnosisSafe safe, Budget budget, Roles roles) = factory.createFirm(address(this));
+        assertTrue(safe.isModuleEnabled(address(budget)));
+        assertTrue(roles.hasRootRole(address(safe)));
+    }
+
+    function testExecutingPaymentsFromBudget() public {
+        (GnosisSafe safe, Budget budget,) = factory.createFirm(address(this));
+        token.mint(address(safe), 100);
+
+        address spender = address(10);
+        address receiver = address(11);
+        hevm.prank(address(safe));
+        uint256 allowanceId = budget.createAllowance(
+            spender,
+            address(token),
+            10,
+            TimeShiftLib.TimeShift(TimeShiftLib.TimeUnit.Daily, 0).encode()
+        );
+
+        hevm.startPrank(spender);
+        budget.executePayment(allowanceId, receiver, 4);
+        budget.executePayment(allowanceId, receiver, 1);
+
+        hevm.warp(block.timestamp + 1 days);
+        budget.executePayment(allowanceId, receiver, 9);
+
+        hevm.expectRevert(abi.encodeWithSelector(Budget.Overbudget.selector, allowanceId, address(token), receiver, 2, 1));
+        budget.executePayment(allowanceId, receiver, 2);
+
+        assertEq(token.balanceOf(receiver), 14);
+    }
+}
