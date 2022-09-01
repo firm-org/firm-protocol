2b4349ae02b66d067200bda5265b4d8d0799b818
Jorge Izquierdo <jorge@izqui.me>
2022-09-01 09:30:03 +0100

    Scripts: Testinprod

--- scripts/TestinprodDeploy.sol
+++ scripts/TestinprodDeploy.sol
@@ -6,27 +6,38 @@ import 'forge-std/Test.sol';
 import "gnosis-safe/GnosisSafe.sol";
 import "gnosis-safe/proxies/GnosisSafeProxyFactory.sol";
 
-import {FirmFactory, UpgradeableModuleProxyFactory} from "../src/factory/FirmFactory.sol";
+import {TestinprodFactory, UpgradeableModuleProxyFactory} from "../src/factory/TestinprodFactory.sol";
 import {Roles, IRoles, IAvatar, ONLY_ROOT_ROLE} from "../src/roles/Roles.sol";
 import {Budget, TimeShiftLib, NO_PARENT_ID} from "../src/budget/Budget.sol";
 
-contract LocalDeploy is Test {
+contract TestinprodDeploy is Test {
+    error UnsupportedChain(uint256 chainId);
 
-    FirmFactory factory;
+    function run() public returns (TestinprodFactory factory) {
+        // using v1.3.0 from https://github.com/safe-global/safe-deployments/blob/8dea757/src/assets/v1.3.0/proxy_factory.json
+        address safeProxyFactory;
+        address safeImpl;
+        
+        if (block.chainid == 1) {
+            // Mainnet
+            safeProxyFactory = 0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2;
+            safeImpl = 0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552;
+        } else if (block.chainid == 5) {
+            // Goerli
+            safeProxyFactory = 0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2;
+            safeImpl = 0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552;
+        } else {
+            revert UnsupportedChain(block.chainid);
+        }
 
-    function run() public returns (FirmFactory) {
-        vm.startBroadcast();
-
-        factory = new FirmFactory(
-            new GnosisSafeProxyFactory(),
+        factory = new TestinprodFactory(
+            GnosisSafeProxyFactory(safeProxyFactory),
             new UpgradeableModuleProxyFactory(),
-            address(new GnosisSafe()),
+            safeImpl,
             address(new Roles(IAvatar(address(10)))),
             address(new Budget(IAvatar(address(10)), IRoles(address(10))))
         );
 
         vm.stopBroadcast();
-
-        return factory;
     }
 }
