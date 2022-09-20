e61a2b13c35793356290ca640d28a315d3ccdc69
Jorge Izquierdo <j@firm.org>
2022-09-20 19:49:39 +0200

    FirmRelayer: move RelayTarget

--- src/metatx/test/FirmRelayer.t.sol
+++ src/metatx/test/FirmRelayer.t.sol
@@ -4,27 +4,7 @@ pragma solidity 0.8.16;
 import {FirmTest} from "../../common/test/lib/FirmTest.sol";
 
 import "../FirmRelayer.sol";
-
-import {ERC2771Context} from "../../bases/ERC2771Context.sol";
-contract RelayTarget is ERC2771Context {
-    error BadSender(address expected, address actual);
-
-    address public lastSender;
-
-    constructor(address trustedForwarder) {
-        _setTrustedForwarder(trustedForwarder, true);
-    }
-
-    function onlySender(address expectedSender) public returns (address sender) {
-        sender = _msgSender();
-
-        if (sender != expectedSender) {
-            revert BadSender(expectedSender, _msgSender());
-        }
-
-        lastSender = sender;
-    }
-}
+import "./mocks/RelayTarget.sol";
 
 contract FirmRelayerTest is FirmTest {
     FirmRelayer relayer;
---
+++ src/metatx/test/mocks/RelayTarget.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.16;
+
+import {ERC2771Context} from "../../../bases/ERC2771Context.sol";
+
+contract RelayTarget is ERC2771Context {
+    error BadSender(address expected, address actual);
+
+    address public lastSender;
+
+    constructor(address trustedForwarder) {
+        _setTrustedForwarder(trustedForwarder, true);
+    }
+
+    function onlySender(address expectedSender) public returns (address sender) {
+        sender = _msgSender();
+
+        if (sender != expectedSender) {
+            revert BadSender(expectedSender, _msgSender());
+        }
+
+        lastSender = sender;
+    }
+}
