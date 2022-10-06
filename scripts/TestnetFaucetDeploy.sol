e138750c8c869956fa1b18e19d9a5a730fc877bd
Jorge Izquierdo <j@firm.org>
2022-10-06 12:59:29 +0200

    Implement Testnet Token Faucet

---
+++ scripts/TestnetFaucetDeploy.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.16;
+
+import "forge-std/Test.sol";
+
+import {TestnetTokenFaucet} from "../src/testnet/TestnetTokenFaucet.sol";
+
+contract TestnetFaucetDeploy is Test {
+    function run() public {
+        vm.startBroadcast();
+        
+        TestnetTokenFaucet faucet = new TestnetTokenFaucet();
+
+        faucet.create("USD Coin", "USDC", 6);
+        faucet.create("Tether USD", "USDT", 6);
+        faucet.create("Euro Coin", "EUROC", 6);
+        faucet.create("Dai Stablecoin", "DAI", 18);
+        faucet.create("Wrapped BTC", "WBTC", 8);
+        faucet.create("Uniswap", "UNI", 18);
+        faucet.create("ChainLink Token", "LINK", 18);
+
+        vm.stopBroadcast();
+    }
+}
