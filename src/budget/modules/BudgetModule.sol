cd05c50d6aa83ea6ec5eeaa42a08fdd901736069
Jorge Izquierdo <j@firm.org>
2022-10-07 15:11:09 +0200

    BudgetModule: initialize on constructor

--- src/budget/modules/BudgetModule.sol
+++ src/budget/modules/BudgetModule.sol
@@ -4,11 +4,19 @@ pragma solidity 0.8.16;
 import {Budget} from "../Budget.sol";
 import {FirmBase, IAvatar} from "../../bases/FirmBase.sol";
 
+address constant IMPL_INIT_ADDRESS = address(1);
+
 abstract contract BudgetModule is FirmBase {
     Budget public budget; // TODO: Unstructured storage
 
+    constructor() {
+        // Initialize with impossible values in constructor so impl base cannot be used
+        initialize(Budget(IMPL_INIT_ADDRESS), IMPL_INIT_ADDRESS);
+    }
+
     function initialize(Budget budget_, address trustedForwarder_) public {
-        __init_firmBase(budget_.safe(), trustedForwarder_);
+        IAvatar safe = address(budget_) != IMPL_INIT_ADDRESS ? budget_.safe() : IAvatar(IMPL_INIT_ADDRESS);
+        __init_firmBase(safe, trustedForwarder_);
         budget = budget_;
     }
 
