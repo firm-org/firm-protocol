96630525d34dc2bb4f54aa51db2ecfde872023e6
Jorge Izquierdo <j@firm.org>
2023-01-15 20:26:05 +0100

    FirmFactory: add integration test

--- src/factory/test/FirmFactoryIntegrationTest.t.sol
+++ src/factory/test/FirmFactoryIntegrationTest.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.16;
 
-import {GnosisSafe} from "gnosis-safe/GnosisSafe.sol";
+import {GnosisSafe, OwnerManager} from "gnosis-safe/GnosisSafe.sol";
 
 import {FirmTest} from "src/common/test/lib/FirmTest.sol";
 import {roleFlag} from "src/common/test/mocks/RolesAuthMock.sol";
@@ -9,7 +9,12 @@ import {ModuleMock} from "src/common/test/mocks/ModuleMock.sol";
 
 import {Budget, TimeShiftLib, NO_PARENT_ID, INHERITED_AMOUNT} from "src/budget/Budget.sol";
 import {TimeShift} from "src/budget/TimeShiftLib.sol";
-import {Roles, IRoles, ISafe, ONLY_ROOT_ROLE_AS_ADMIN, ROOT_ROLE_ID} from "src/roles/Roles.sol";
+import {Roles, IRoles, ISafe, ONLY_ROOT_ROLE_AS_ADMIN, ROOT_ROLE_ID, SAFE_OWNER_ROLE_ID} from "src/roles/Roles.sol";
+import {Captable, EquityToken, IBouncer, NO_CONVERSION_FLAG} from "src/captable/Captable.sol";
+import {Voting} from "src/voting/Voting.sol";
+import {EmbeddedBouncerType, EMBEDDED_BOUNCER_FLAG_TYPE} from "src/captable/BouncerChecker.sol";
+import {AddressUint8FlagsLib} from "src/common/AddressUint8FlagsLib.sol";
+
 import {FirmRelayer} from "src/metatx/FirmRelayer.sol";
 import {BokkyPooBahsDateTimeLibrary as DateTimeLib} from "datetime/BokkyPooBahsDateTimeLibrary.sol";
 
@@ -26,6 +31,7 @@ string constant LLAMAPAYSTREAMS_MODULE_ID = "org.firm.budget.llamapay-streams";
 contract FirmFactoryIntegrationTest is FirmTest {
     using TimeShiftLib for *;
     using ForwarderLib for ForwarderLib.Forwarder;
+    using AddressUint8FlagsLib for *;
 
     FirmFactory factory;
     UpgradeableModuleProxyFactory moduleFactory;
@@ -70,7 +76,10 @@ contract FirmFactoryIntegrationTest is FirmTest {
         assertTrue(budget.isTrustedForwarder(address(relayer)));
     }
 
-    function createFirmWithRoleAndAllowance(address spender) internal returns (GnosisSafe safe, Budget budget, Roles roles) {
+    function createFirmWithRoleAndAllowance(address spender)
+        internal
+        returns (GnosisSafe safe, Budget budget, Roles roles)
+    {
         address[] memory safeOwners = new address[](1);
         safeOwners[0] = address(this);
         FirmFactory.SafeConfig memory safeConfig = FirmFactory.SafeConfig(safeOwners, 1);
@@ -84,10 +93,8 @@ contract FirmFactoryIntegrationTest is FirmTest {
         address[] memory grantees = new address[](1);
         grantees[0] = spender;
         FirmFactory.RoleCreationInput[] memory rolesCreationInput = new FirmFactory.RoleCreationInput[](1);
-        rolesCreationInput[0] = FirmFactory.RoleCreationInput(
-            ONLY_ROOT_ROLE_AS_ADMIN, "Executive", grantees
-        );
-        
+        rolesCreationInput[0] = FirmFactory.RoleCreationInput(ONLY_ROOT_ROLE_AS_ADMIN, "Executive", grantees);
+
         FirmFactory.CaptableConfig memory captableConfig;
         FirmFactory.VotingConfig memory votingConfig;
         FirmFactory.FirmConfig memory firmConfig = FirmFactory.FirmConfig({
@@ -104,8 +111,8 @@ contract FirmFactoryIntegrationTest is FirmTest {
     function testExecutingPaymentsFromBudget() public {
         (address spender, uint256 spenderPk) = accountAndKey("spender");
         address receiver = account("receiver");
-        
-        (GnosisSafe safe, Budget budget, Roles roles) = createFirmWithRoleAndAllowance(spender);
+
+        (GnosisSafe safe, Budget budget,) = createFirmWithRoleAndAllowance(spender);
         uint256 allowanceId = budget.allowancesCount();
 
         token.mint(address(safe), 100);
@@ -153,8 +160,7 @@ contract FirmFactoryIntegrationTest is FirmTest {
             FirmRelayer.Assertion[] memory assertions = new FirmRelayer.Assertion[](1);
             assertions[0] = FirmRelayer.Assertion({position: 0, expectedValue: bytes32(newAllowanceId)});
 
-            request =
-                FirmRelayer.RelayRequest({from: spender, nonce: 0, calls: calls, assertions: assertions});
+            request = FirmRelayer.RelayRequest({from: spender, nonce: 0, calls: calls, assertions: assertions});
         }
 
         relayer.relay(request, _signPacked(relayer.requestTypedDataHash(request), spenderPk));
@@ -226,6 +232,102 @@ contract FirmFactoryIntegrationTest is FirmTest {
         assertApproxEqAbs(token.balanceOf(receiver), 15_000 * 10 ** token.decimals(), 2);
     }
 
+    address shareholder1 = account("Shareholder 1");
+    address shareholder2 = account("Shareholder 2");
+    uint256 shareholder1Shares = 60;
+    uint256 shareholder2Shares = 40;
+    function createFirmWithCaptableVotingAndAllowance() internal returns (GnosisSafe safe, Budget budget, Roles roles, Voting voting, Captable captable) {
+        address[] memory safeOwners = new address[](1);
+        safeOwners[0] = address(this);
+        FirmFactory.SafeConfig memory safeConfig = FirmFactory.SafeConfig(safeOwners, 1);
+
+        FirmFactory.ClassCreationInput[] memory classes = new FirmFactory.ClassCreationInput[](1);
+        classes[0] = FirmFactory.ClassCreationInput({
+            className: "Common",
+            ticker: "TST-A",
+            authorized: shareholder1Shares + shareholder2Shares,
+            convertsToClassId: NO_CONVERSION_FLAG,
+            votingWeight: 1,
+            bouncer: IBouncer(uint8(EmbeddedBouncerType.AllowAll).toFlag(EMBEDDED_BOUNCER_FLAG_TYPE))
+        });
+        FirmFactory.ShareIssuanceInput[] memory shareIssuances = new FirmFactory.ShareIssuanceInput[](2);
+        shareIssuances[0] = FirmFactory.ShareIssuanceInput(0, shareholder1, shareholder1Shares);
+        shareIssuances[1] = FirmFactory.ShareIssuanceInput(0, shareholder2, shareholder2Shares);
+        FirmFactory.CaptableConfig memory captableConfig = FirmFactory.CaptableConfig("TestCo", classes, shareIssuances);
+        FirmFactory.VotingConfig memory votingConfig = FirmFactory.VotingConfig({
+            quorumNumerator: 5000,
+            votingDelay: 1,
+            votingPeriod: 1,
+            proposalThreshold: 1
+        });
+        FirmFactory.AllowanceCreationInput[] memory allowances = new FirmFactory.AllowanceCreationInput[](1);
+        // Create an allowance which allows safe owners to spend 10 tokens daily
+        allowances[0] = FirmFactory.AllowanceCreationInput(
+            roleFlag(SAFE_OWNER_ROLE_ID), address(token), 10, TimeShift(TimeShiftLib.TimeUnit.Daily, 0).encode(), ""
+        );
+        FirmFactory.RoleCreationInput[] memory noRolesInput = new FirmFactory.RoleCreationInput[](0);
+        FirmFactory.FirmConfig memory firmConfig = FirmFactory.FirmConfig({
+            withCaptableAndVoting: true,
+            budgetConfig: FirmFactory.BudgetConfig(allowances),
+            rolesConfig: FirmFactory.RolesConfig(noRolesInput),
+            captableConfig: captableConfig,
+            votingConfig: votingConfig
+        });
+        return getFirmAddressesWithCaptableVoting(factory.createFirm(safeConfig, firmConfig, 1));
+    }
+
+    function testExecutingProposalsFromVoting() public {
+        vm.roll(1);
+        (GnosisSafe safe, Budget budget, Roles roles, Voting voting, Captable captable) = createFirmWithCaptableVotingAndAllowance();
+        token.mint(address(safe), 100);
+        
+        (EquityToken equityToken,,,,,,,,) = captable.classes(0);
+
+        assertEq(equityToken.balanceOf(shareholder1), shareholder1Shares);
+        assertEq(equityToken.balanceOf(shareholder2), shareholder2Shares);
+
+        vm.prank(shareholder1);
+        equityToken.delegate(shareholder1);
+        vm.prank(shareholder2);
+        equityToken.delegate(shareholder2);
+        blocktravel(1);
+
+        bytes memory proposalData = abi.encodeCall(
+            OwnerManager.addOwnerWithThreshold, (shareholder1, 1)
+        );
+        _createAndExecuteProposal(voting, address(safe), 0, proposalData);
+
+        // now, as safe owner, shareholder 1 has permission to execute budget payments
+        uint256 allowanceId = budget.allowancesCount();
+        vm.prank(shareholder1);
+        budget.executePayment(allowanceId, shareholder2, 1, "Test");
+
+        assertEq(token.balanceOf(shareholder2), 1);
+    }
+
+     function _createAndExecuteProposal(
+        Voting voting,
+        address to,
+        uint256 value,
+        bytes memory data
+    ) internal {
+        blocktravel(1);
+        vm.prank(shareholder1);
+        string memory description = "Test";
+        uint256 proposalId = voting.propose(arr(address(to)), arr(value), arr(data), description);
+
+        blocktravel(2);
+
+        vm.prank(shareholder1);
+        voting.castVote(proposalId, 1);
+        vm.prank(shareholder2);
+        voting.castVote(proposalId, 0);
+
+        blocktravel(2);
+
+        voting.execute(arr(address(to)), arr(value), arr(data), keccak256(bytes(description)));
+    }
+
     function createBarebonesFirm(address owner) internal returns (GnosisSafe safe, Budget budget, Roles roles) {
         return getFirmAddresses(factory.createBarebonesFirm(owner, 1));
     }
@@ -240,6 +342,20 @@ contract FirmFactoryIntegrationTest is FirmTest {
         vm.label(address(roles), "RolesProxy");
     }
 
+    function getFirmAddressesWithCaptableVoting(GnosisSafe safe) internal returns (GnosisSafe _safe, Budget budget, Roles roles, Voting voting, Captable captable) {
+        (address[] memory modules,) = safe.getModulesPaginated(address(0x1), 2);
+        budget = Budget(modules[1]);
+        roles = Roles(address(budget.roles()));
+        _safe = safe;
+        voting = Voting(payable(address(modules[0])));
+        captable = Captable(address(voting.token()));
+
+        vm.label(address(budget), "BudgetProxy");
+        vm.label(address(roles), "RolesProxy");
+        vm.label(address(voting), "VotingProxy");
+        vm.label(address(captable), "CaptableProxy");
+    }
+
     function _signPacked(bytes32 hash, uint256 pk) internal pure returns (bytes memory sig) {
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(pk, hash);
 
@@ -249,5 +365,20 @@ contract FirmFactoryIntegrationTest is FirmTest {
             mstore(add(sig, 0x40), s)
             mstore8(add(sig, 0x60), v)
         }
+    }
+
+    function arr(address a) internal pure returns (address[] memory _arr) {
+        _arr = new address[](1);
+        _arr[0] = a;
+    }
+
+    function arr(bytes memory a) internal pure returns (bytes[] memory _arr) {
+        _arr = new bytes[](1);
+        _arr[0] = a;
+    }
+
+    function arr(uint256 a) internal pure returns (uint256[] memory _arr) {
+        _arr = new uint256[](1);
+        _arr[0] = a;
     }
 }
