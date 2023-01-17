da9cfa005388a0934b6e57efb2cb7a9e50759b0c
Jorge Izquierdo <j@firm.org>
2023-01-17 16:05:14 +0100

    EquityToken: add tests (#150)
    * Captable: add test for decimals
    
    * Equity token: add tests
    
    * Equity token: more tests

--- src/captable/test/EquityToken.t.sol
+++ src/captable/test/EquityToken.t.sol
@@ -1,17 +1,68 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.16;
 
-import {FirmTest} from "../../common/test/lib/FirmTest.sol";
+import {BaseCaptableTest} from "./Captable.t.sol";
 
-import {Captable} from "../Captable.sol";
+import {Captable, NO_CONVERSION_FLAG} from "../Captable.sol";
 import {EquityToken} from "../EquityToken.sol";
 
-contract EquityTokenTest is FirmTest {
-    Captable captable;
+contract EquityTokenTest is BaseCaptableTest {
+    EquityToken token;
+    uint256 classId;
 
-    /*
-    function setUp() public {
-        captable = new Captable();
+    uint256 HOLDER_BALANCE = 1000;
+
+    function setUp() public override {
+        super.setUp();
+
+        vm.startPrank(address(safe));
+        (classId, token) = captable.createClass("Common", "TST-A", 10000, NO_CONVERSION_FLAG, 1, ALLOW_ALL_BOUNCER);
+        captable.issue(HOLDER1, classId, HOLDER_BALANCE);
+        vm.stopPrank();
     }
-    */
+
+    function testInitialState() public {
+        assertEq(token.classId(), classId);
+        assertEq(address(token.captable()), address(captable));
+        assertEq(token.totalSupply(), HOLDER_BALANCE);
+        assertEq(token.decimals(), 18);
+    }
+
+    function testCantReinitialize() public {
+        vm.expectRevert(abi.encodeWithSelector(EquityToken.AlreadyInitialized.selector));
+        token.initialize(captable, 1);
+    }
+
+    function testCaptableCanPerformAdminActions() public {
+        vm.startPrank(address(captable));
+
+        token.mint(HOLDER2, 2);
+        assertEq(token.balanceOf(HOLDER2), 2);
+
+        token.burn(HOLDER2, 1);
+        assertEq(token.balanceOf(HOLDER2), 1);
+
+        token.forcedTransfer(HOLDER2, HOLDER1, 1);
+        assertEq(token.balanceOf(HOLDER1), HOLDER_BALANCE + 1);
+        assertEq(token.balanceOf(HOLDER2), 0);
+        
+        vm.stopPrank();
+    }
+
+    function testNonCaptableCannotPerformAdminActions() public {
+        bytes memory onlyCaptableError = abi.encodeWithSelector(EquityToken.UnauthorizedNotCaptable.selector);
+        
+        vm.startPrank(address(safe));
+
+        vm.expectRevert(onlyCaptableError);
+        token.mint(HOLDER1, 1);
+
+        vm.expectRevert(onlyCaptableError);
+        token.burn(HOLDER1, 1);
+
+        vm.expectRevert(onlyCaptableError);
+        token.forcedTransfer(HOLDER1, HOLDER2, 1);
+
+        vm.stopPrank();
+    }
 }
